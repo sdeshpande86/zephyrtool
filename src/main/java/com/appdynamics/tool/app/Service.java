91879b47d3c9f07095e8103d63822ba2c3893928
Prudhvi Chaganti <prudhvi.chaganti@appdynamics.com>
2016-05-25 20:21:54 -0700

    Adding more endpoints

--- src/main/java/com/appdynamics/tool/app/Service.java
+++ src/main/java/com/appdynamics/tool/app/Service.java
@@ -5,8 +5,18 @@ import static spark.Spark.get;
 public class Service {
 	public static void main(String[] args) throws InterruptedException {
 		App.initialize();
+
 		get("/zephyrtool/getfeatures", (request, response) -> {
-				return App.gson.toJson(App.usecaseFeaturesMap.get("Technical Debt"));
+			String usecase = request.queryParams("usecase");
+			if (usecase != null && !usecase.isEmpty()) {
+				return App.gson.toJson(App.usecaseFeaturesMap.get(usecase));
+			} else {
+				return App.gson.toJson(App.usecaseFeaturesMap);
+			}
+		});
+		
+		get("zephyrtool/getallusecases", (request, response) -> {
+			return App.gson.toJson(App.usecases);
 		});
 	}
 }
