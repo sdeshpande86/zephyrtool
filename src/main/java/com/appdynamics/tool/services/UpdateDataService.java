9b05a157c12447a22bde6929bf19ac5ebb3aac97
Prudhvi Chaganti <prudhvi.chaganti@appdynamics.com>
2016-05-27 10:15:22 -0700

    Adding update data service

---
+++ src/main/java/com/appdynamics/tool/services/UpdateDataService.java
@@ -0,0 +1,19 @@
+package com.appdynamics.tool.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.appdynamics.tool.app.App;
+
+@Path("updatedata")
+public class UpdateDataService {
+	@GET
+	@Produces("application/json")
+	public String updateData() throws InterruptedException {
+		App.usecases.clear();
+		App.usecaseFeaturesMap.clear();
+		App.initialize();
+		return "Updated data successfully";
+ 	}
+}
