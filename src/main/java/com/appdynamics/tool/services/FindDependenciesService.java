e9cc6cc5a33b682ebf0bfff1e7153322bb8a24aa
Prudhvi Chaganti <prudhvi.chaganti@appdynamics.com>
2016-08-09 17:49:14 -0700

    Updating FindDependenciesService to capture dependencies from multiple levels

--- src/main/java/com/appdynamics/tool/services/FindDependenciesService.java
+++ src/main/java/com/appdynamics/tool/services/FindDependenciesService.java
@@ -1,7 +1,9 @@
 package com.appdynamics.tool.services;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,16 +19,31 @@ public class FindDependenciesService {
 	@GET
 	@Produces("application/json")
 	public String findDependencies(@QueryParam("issueKey") String issueKey) {
-		List<String> dependentIssueKeys = new ArrayList<String>();
-		String output = App.sendRequestNew("/rest/api/2/issue/" + issueKey);
-		JsonObject issueJson = App.parser.parse(output).getAsJsonObject();
-		JsonObject fields = issueJson.get("fields").getAsJsonObject();
-		JsonArray issueLinks = fields.get("issuelinks").getAsJsonArray();
-		for (int j = 0; j < issueLinks.size(); j++) {
-			if (issueLinks.get(j).getAsJsonObject().has("outwardIssue") && issueLinks.get(j).getAsJsonObject().get("type").getAsJsonObject().get("outward").getAsString().equalsIgnoreCase("blocks")) {
-				dependentIssueKeys.add(issueLinks.get(j).getAsJsonObject().get("outwardIssue").getAsJsonObject().get("key").getAsString());
-			}
-		}
+		Set<String> dependentIssueKeys = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+		Set<String> intermediateSet = new HashSet<String>();
+		intermediateSet.add(issueKey);
+		getChildren(dependentIssueKeys, intermediateSet);
+		dependentIssueKeys.remove(issueKey);
 		return App.gson.toJson(dependentIssueKeys);
+	}
+	
+	private void getChildren(Set<String> dependentIssueKeys, Set<String> intermediateSet) {
+		for (String issueKey : intermediateSet) {
+			Set<String> temp = new HashSet<String>();
+			dependentIssueKeys.add(issueKey);
+			String output = App.sendRequestNew("/rest/api/2/issue/" + issueKey);
+			JsonObject issueJson = App.parser.parse(output).getAsJsonObject();
+			JsonObject fields = issueJson.get("fields").getAsJsonObject();
+			JsonArray issueLinks = fields.get("issuelinks").getAsJsonArray();
+			for (int j = 0; j < issueLinks.size(); j++) {
+				if (issueLinks.get(j).getAsJsonObject().has("outwardIssue") && issueLinks.get(j).getAsJsonObject().get("type").getAsJsonObject().get("outward").getAsString().equalsIgnoreCase("blocks")) {
+					String dependentIssueKey = issueLinks.get(j).getAsJsonObject().get("outwardIssue").getAsJsonObject().get("key").getAsString();
+					if (!dependentIssueKeys.contains(dependentIssueKey)) {
+						temp.add(dependentIssueKey);
+					}
+				}
+			}
+			getChildren(dependentIssueKeys, temp);
+		}
 	}
 }
